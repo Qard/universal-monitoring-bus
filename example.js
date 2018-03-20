351da185a657839e00e253f8a51f0aadb732e414
Stephen Belanger <stephen.belanger@elastic.co>
2018-03-19 20:33:24 -0700

    Add span aggregator

--- example.js
+++ example.js
@@ -1,5 +1,6 @@
 const { monitor, Span, Metric, PullMetric } = require('./')
 const TransactionAggregator = require('./lib/aggregators/spans/transaction')
+const SpanAggregator = require('./lib/aggregators/spans/span')
 
 
 //
@@ -9,36 +10,43 @@ const watch = monitor.watch({
   type: 'span',
   namespace: 'test',
   data: { foo: 'bar' }
-})
+}, () => {})
 
 const span = new Span('test', { foo: 'bar' })
-console.log('is watching', span, monitor.watching(span))
+const start = span.start()
+console.log('is watching', start, monitor.watching(start))
 
 watch.stop()
-console.log('is watching', span, monitor.watching(span))
+console.log('is watching', start, monitor.watching(start))
 
 
 //
 // Pull-based metric collectors
 //
-const rss = new PullMetric('rss', null, 1000, () => {
-  return process.memoryUsage().rss
-})
-const heapTotal = new PullMetric('heap-total', null, 1000, () => {
-  return process.memoryUsage().heapTotal
-})
-const heapUsed = new PullMetric('heap-used', null, 1000, () => {
-  return process.memoryUsage().heapUsed
-})
+// const rss = new PullMetric('rss', null, 1000, () => {
+//   return process.memoryUsage().rss
+// })
+// const heapTotal = new PullMetric('heap-total', null, 1000, () => {
+//   return process.memoryUsage().heapTotal
+// })
+// const heapUsed = new PullMetric('heap-used', null, 1000, () => {
+//   return process.memoryUsage().heapUsed
+// })
 
 //
 // Spans
 //
-const transactions = new TransactionAggregator(transaction => {
-  console.log('done', JSON.stringify(transaction, null, 2))
+// const transactions = new TransactionAggregator(transaction => {
+//   console.log('done', JSON.stringify(transaction, null, 2))
+// })
+// monitor.watch({ type: 'span' }, span => {
+//   transactions.send(span)
+// })
+const spans = new SpanAggregator(span => {
+  console.log('span', JSON.stringify(span, null, 2))
 })
 monitor.watch({ type: 'span' }, span => {
-  transactions.send(span)
+  spans.send(span)
 })
 
 async function makeSpan() {
@@ -52,9 +60,17 @@ async function makeSpan() {
 
   await delay(randScale(1000))
 
+  if (monitor.watching({ type: 'span', namespace: 'another-span' })) {
+    let inner = new Span('another-span', {
+      foo: 'bar'
+    })
+    inner.start(span.$start)
+    inner.end()
+  }
+
   if (span) {
     for (let n = 0; n < 5; n++) {
-      span.log(`message #${n}`)
+      span.log('message #%d', n)
       await delay(randScale(100))
     }
     span.annotate({ foo: 'bar' })
